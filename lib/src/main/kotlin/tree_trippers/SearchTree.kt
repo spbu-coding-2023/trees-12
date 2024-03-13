6d0184740fd8291e6dad709213b70acf5ab994f7
IliaSuponeff <suponeff.i@gmail.com>
2024-03-14 01:09:05 +0300

    Update search-tree interface and simple init by abstract binary search tree properties as root and tree size

--- lib/src/main/kotlin/tree_trippers/SearchTree.kt
+++ lib/src/main/kotlin/tree_trippers/SearchTree.kt
@@ -3,7 +3,5 @@ package tree_trippers
 import tree_trippers.nodes.SearchTreeNode
 
 public interface SearchTree<K: Comparable<K>, V, N: SearchTreeNode<K, V, N>> {
-
-
-
+    public fun size(): Int
 }
--- lib/src/main/kotlin/tree_trippers/binary_trees/AbstractBSTree.kt
+++ lib/src/main/kotlin/tree_trippers/binary_trees/AbstractBSTree.kt
@@ -4,4 +4,10 @@ import tree_trippers.SearchTree
 import tree_trippers.nodes.binary_nodes.AbstractBSTreeNode
 
 abstract class AbstractBSTree<K: Comparable<K>, V, N: AbstractBSTreeNode<K, V, N>>: SearchTree<K, V, N> {
+    protected var root: N? = null
+    private var size: Int = 0
+
+    override fun size(): Int {
+        return size
+    }
 }
