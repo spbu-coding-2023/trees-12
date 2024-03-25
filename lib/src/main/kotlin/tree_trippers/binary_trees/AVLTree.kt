742427fbcc337067046e97836a25b703ef99190c
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-03-25 23:02:26 +0300

    docs(AVLTree & AVLTreeNode): add doc-strings

--- lib/src/main/kotlin/tree_trippers/binary_trees/AVLTree.kt
+++ lib/src/main/kotlin/tree_trippers/binary_trees/AVLTree.kt
@@ -3,6 +3,14 @@ package tree_trippers.binary_trees
 import tree_trippers.nodes.binary_nodes.AVLTreeNode
 
 
+/**
+ * AVLTree class represents a self-balancing binary search tree that maintains
+ * the property of AVL tree, ensuring that the heights
+ * of the two child subtrees of any node differ by at most one.
+ *
+ * @param K the type of the keys in the tree
+ * @param V the value type associated with the key
+ */
 public class AVLTree<K: Comparable<K>, V>: AbstractBSTree<K, V, AVLTreeNode<K, V>>() {
 
     override fun createNode(key: K, value: V): AVLTreeNode<K, V> {
@@ -14,10 +22,20 @@ public class AVLTree<K: Comparable<K>, V>: AbstractBSTree<K, V, AVLTreeNode<K, V
         return balance(node)
     }
 
+    /**
+     * @param node the AVLTreeNode for which to calculate the balance factor
+     * @return balance factor (height difference of his children) of the [node]
+     */
     private fun balanceFactor(node: AVLTreeNode<K, V>?): Int {
         return (node?.rightChild?.height ?: 0) - (node?.leftChild?.height ?: 0)
     }
 
+    /**
+     * Defines and calls the method(s) for balancing the current [node].
+     *
+     * @param node the node to balance in the AVL tree
+     * @return the root of the rebalanced subtree or a [node] if balance is not required
+     */
     private fun balance(node: AVLTreeNode<K, V>): AVLTreeNode<K, V> {
         when (balanceFactor(node)) {
             -2 -> {
@@ -34,6 +52,13 @@ public class AVLTree<K: Comparable<K>, V>: AbstractBSTree<K, V, AVLTreeNode<K, V
         }
     }
 
+    /**
+     * Performs a left rotation (counterclockwise) of the tree with the [node] as the root.
+     * Updates the heights of the affected nodes.
+     *
+     * @param node the node to perform a left rotation on
+     * @returns the root of the rotated subtree
+     */
     private fun rotateLeft(node: AVLTreeNode<K, V>): AVLTreeNode<K, V> {
         val nodeSwapped = node.rightChild ?: return node
         node.rightChild = nodeSwapped.leftChild
@@ -44,6 +69,13 @@ public class AVLTree<K: Comparable<K>, V>: AbstractBSTree<K, V, AVLTreeNode<K, V
         return nodeSwapped
     }
 
+    /**
+     * Performs a right rotation (clockwise) of the tree with the [node] as the root.
+     * Updates the heights of the affected nodes.
+     *
+     * @param node the node to perform a right rotation on
+     * @returns the root of the rotated subtree
+     */
     private fun rotateRight(node: AVLTreeNode<K, V>): AVLTreeNode<K, V> {
         val nodeSwapped = node.leftChild ?: return node
         node.leftChild = nodeSwapped.rightChild
--- lib/src/main/kotlin/tree_trippers/nodes/binary_nodes/AVLTreeNode.kt
+++ lib/src/main/kotlin/tree_trippers/nodes/binary_nodes/AVLTreeNode.kt
@@ -2,13 +2,23 @@ package tree_trippers.nodes.binary_nodes
 
 import kotlin.math.max
 
-
+/**
+ * AVLTreeNode class represents a node in an AVL tree, containing a key-value pair and additional
+ * information for maintaining the AVL property such as the height of the node.
+ *
+ * @param K the key type that implements the Comparable interface
+ * @param V the value type associated with the key
+ * @property height the height of the [AVLTreeNode] in the AVL tree
+ */
 public class AVLTreeNode<K: Comparable<K>, V>(
     key: K,
     value: V
 ): AbstractBSTreeNode<K, V, AVLTreeNode<K, V>>(key, value) {
+
+    /** The height of the node in the AVL tree, initialized to 1. */
     public var height: Int = 1
 
+    /** Updates height of the node in AVL tree based on the heights of its left and right child subtrees. */
     public fun updateHeight() {
         val leftHeight = this.leftChild?.height ?: 0
         val rightHeight = this.rightChild?.height ?: 0
