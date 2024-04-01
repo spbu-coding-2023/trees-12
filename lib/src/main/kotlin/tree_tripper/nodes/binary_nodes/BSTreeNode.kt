0d56925dd79736293325f13ca1325f1fdfebe1b1
IliaSuponeff <suponeff.i@gmail.com>
2024-04-01 13:54:32 +0300

    refactor: add new constructor of simple BSTreeNode by adding params as left and right child

--- lib/src/main/kotlin/tree_tripper/nodes/binary_nodes/BSTreeNode.kt
+++ lib/src/main/kotlin/tree_tripper/nodes/binary_nodes/BSTreeNode.kt
@@ -7,7 +7,16 @@ package tree_tripper.nodes.binary_nodes
  * @param K the key type of the node, supporting the [Comparable] interface
  * @param V the value type of the node
  */
-public class BSTreeNode<K: Comparable<K>, V>(
+public class BSTreeNode<K : Comparable<K>, V>(
     key: K,
     value: V
-): AbstractBSTreeNode<K, V, BSTreeNode<K, V>>(key, value)
+) : AbstractBSTreeNode<K, V, BSTreeNode<K, V>>(key, value) {
+
+    public constructor(key: K, value: V, leftChild: BSTreeNode<K, V>?, rightChild: BSTreeNode<K, V>?) : this(
+        key,
+        value
+    ) {
+        this.leftChild = leftChild
+        this.rightChild = rightChild
+    }
+}
